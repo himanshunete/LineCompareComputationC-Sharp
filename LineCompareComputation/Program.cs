0dc9ac898af1c5a08597adfc5501a20386753c91
himanshunete <himanshuneteh@gmail.com>
2021-02-03 11:10:07 +0530

    [himanshunete]Add CheckingLengthOfALine

--- LineCompareComputation/Program.cs
+++ LineCompareComputation/Program.cs
@@ -7,6 +7,14 @@ namespace LineCompareComputation
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Line Comparison Computation Program");
+            //Coordinates
+            double x1 = 4;
+            double y1 = 6;
+            double x2 = 8;
+            double y2 = 7;
+            double squareX = (x2 - x1) * (x2 - x1);
+            double squareY = (y2 - y1) * (y2 - y1);
+            Console.WriteLine($" Length of a Line: { Math.Sqrt(squareX + squareY) }");
         }
     }
 }
